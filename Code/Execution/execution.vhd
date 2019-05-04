965c4475d91d64243675c0e3e63a322eb1090831
sara maher <sara.mh0090@gmail.com>
2019-05-04 19:55:45 +0200

    modified excution

--- Code/Execution/execution.vhd
+++ Code/Execution/execution.vhd
@@ -8,10 +8,11 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity excute is
     generic (n: integer := 16);
   port (enable, rst,clk, c_mem, z_mem, n_mem: std_logic;
-       load_value,  id_ex_src_val, id_ex_dst_val , ex_mem_reg_val, mem_wb_reg_val: in std_logic_vector(n-1 downto 0) ;
-       id_ex_reg_src, id_ex_reg_dst , ex_mem_reg_dst, mem_wb_reg_dst : in std_logic_vector(2 downto 0) ;
+       load_value,  id_ex_src_val, id_ex_dst_val , ex_mem_reg_val, mem_wb_reg_val_alu, mem_wb_reg_val_ld: in std_logic_vector(n-1 downto 0) ;
+       id_ex_reg_src, id_ex_reg_dst , ex_mem_reg_dst, mem_wb_reg_dst_alu, mem_wb_reg_dst_ld : in std_logic_vector(2 downto 0) ;
+       mem_wb_reg_write_1, ex_mem_reg_write_1, mem_wb_reg_write_2: in std_logic;
        load_address: std_logic_vector (2 downto 0);
-       immediate_val: std_logic_vector(6 downto 0);
+       immediate_val: std_logic_vector(4 downto 0);
       opcode_alu, opcode_load: in std_logic_vector (4 downto 0);
             port_wire : inout std_logic_vector(n-1 downto 0) ;
             pcsrc:out std_logic_vector(2 downto 0);
@@ -24,8 +25,7 @@ Architecture a_excute of excute is
     signal buffer_in: std_logic_vector((2*n+11) downto 0);
     signal AluOp_a, AluOp_b, Alu_out: std_logic_vector(n-1 downto 0) ;
     signal AluOp: std_logic_vector (4 downto 0);
-    signal frwd_a, frwd_b: std_logic_vector(1 downto 0);
-    signal mem_wb_reg_write_1, ex_mem_reg_write_1: std_logic;
+    signal frwd_a, frwd_b: std_logic_vector(2 downto 0);
 begin
   AluOp <= opcode_load when (opcode_load = "10001")
       else opcode_alu;
@@ -35,18 +35,19 @@ begin
       opcode_load =>opcode_load, opcode_alu =>opcode_alu,
             id_ex_reg_src => id_ex_reg_src, id_ex_reg_dst =>id_ex_reg_dst,
             ex_mem_reg_dst =>ex_mem_reg_dst,
-            mem_wb_reg_dst => mem_wb_reg_dst,
+            mem_wb_reg_dst1 => mem_wb_reg_dst_alu, mem_wb_reg_dst2 => mem_wb_reg_dst_ld,
             mem_wb_reg_write_1 => mem_wb_reg_write_1, ex_mem_reg_write_1=> ex_mem_reg_write_1,
+            mem_wb_reg_write_2=> mem_wb_reg_write_2,
             frwd_a => frwd_a, frwd_b=>frwd_b);
     
-    mux_a: entity work.mux4x1
+    mux_a: entity work.mux5x1
       generic map(n=> n)
-      port map( a=>id_ex_src_val,b =>ex_mem_reg_val,c=> mem_wb_reg_val, d=>(others=>'0'),
+      port map( a=>id_ex_src_val,b =>ex_mem_reg_val,c=> mem_wb_reg_val_alu, d=>mem_wb_reg_val_ld, e=>(others=>'0'),
         sel =>frwd_a, f => AluOp_a);
     
-    mux_b: entity work.mux4x1
+    mux_b: entity work.mux5x1
       generic map(n=> n)
-      port map( a => id_ex_dst_val,b =>ex_mem_reg_val,c=> mem_wb_reg_val,d=>immediate_val,
+      port map( a => id_ex_dst_val,b =>ex_mem_reg_val,c=>  mem_wb_reg_val_alu,d=>mem_wb_reg_val_ld, e=>immediate_val,
           sel =>frwd_b,
           f=>AluOp_b);
 
--- Code/Execution/frwd_unit.vhd
+++ Code/Execution/frwd_unit.vhd
@@ -10,46 +10,54 @@ entity frwd_unit is -- takes registers address, outs if we forward or not
   port ( clk: in std_logic;
   opcode_load, opcode_alu: in std_logic_vector (4 downto 0);
   id_ex_reg_src, id_ex_reg_dst, ex_mem_reg_dst : in std_logic_vector(n-1 downto 0) ;
-  mem_wb_reg_dst : in std_logic_vector(n-1 downto 0) ;
-  mem_wb_reg_write_1, ex_mem_reg_write_1: in std_logic;          
-  frwd_a, frwd_b: out std_logic_vector(1 downto 0));
+  mem_wb_reg_dst1, mem_wb_reg_dst2 : in std_logic_vector(n-1 downto 0) ;
+  mem_wb_reg_write_1,mem_wb_reg_write_2, ex_mem_reg_write_1: in std_logic;          
+  frwd_a, frwd_b: out std_logic_vector(2 downto 0));
         end ;
         
 Architecture a_frwd_unit of frwd_unit is
-signal mem_raw_a, mem_raw_b, wb_raw_a, wb_raw_b: std_logic;
-signal a,b: std_logic_vector(1 downto 0);
+signal mem_raw_a, mem_raw_b, wb_raw_a1, wb_raw_b1, wb_raw_a2, wb_raw_b2: std_logic;
+signal a,b: std_logic_vector(2 downto 0);
 begin
     -- check if reg a is dst in the instr in mem stage
 mem_raw_a <= '1' when ( id_ex_reg_src = ex_mem_reg_dst)
 else '0';
  -- check if reg a is dst in the instr in wb stage
-wb_raw_a <= '1' when (id_ex_reg_src = mem_wb_reg_dst)
+wb_raw_a1 <= '1' when (id_ex_reg_src = mem_wb_reg_dst1)
+else '0';
+wb_raw_a2 <= '1' when (id_ex_reg_src = mem_wb_reg_dst2)
 else '0';
  -- check if reg b is dst in the instr in mem stage
 mem_raw_b <= '1' when (id_ex_reg_dst = ex_mem_reg_dst)
 else '0';
  -- check if reg b is dst in the instr in wb stage
-wb_raw_b <= '1' when (id_ex_reg_dst = mem_wb_reg_dst)
+wb_raw_b1 <= '1' when (id_ex_reg_dst = mem_wb_reg_dst1)
+else '0';
+wb_raw_b2 <= '1' when (id_ex_reg_dst = mem_wb_reg_dst2)
 else '0';
 
 process (clk )  --i tried working the process with inputs instead of clk but it didn't work
     --variable a,b: std_logic_vector(1 downto 0);
     begin
         if  ex_mem_reg_write_1 ='1' and mem_raw_a ='1' then
-            a<="01";
-        elsif mem_wb_reg_write_1='1' and wb_raw_a='1' then
-            a<="10";
+            a<="001";
+        elsif mem_wb_reg_write_1='1' and wb_raw_a1='1' then
+            a<="010";
+        elsif mem_wb_reg_write_2='1' and wb_raw_a2='1' then
+            a<="011";
         else
-            a<="00";
+            a<="000";
         end if;
         if ex_mem_reg_write_1='1' and mem_raw_b='1' then
-            b<="01";
-        elsif mem_wb_reg_write_1='1' and wb_raw_b='1' then
-            b<="10";
+            b<="001";
+        elsif mem_wb_reg_write_1='1' and wb_raw_b1='1' then
+            b<="010";
+        elsif mem_wb_reg_write_2='1' and wb_raw_b2='1' then
+            b<="011";
         elsif opcode_load = "10001" or opcode_alu ="01101" or opcode_alu = "01110" then 
-            b<="11";
+            b<="100";
         else
-            b<="00";
+            b<="000";
         end if;
 end process; 
 frwd_a <= a;
